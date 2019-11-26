416fb1ce26cfcfd95f6617d0a653a495428cd608
段武杰 <wjduan@linx-info.com>
2019-11-26 17:28:53 +0800

    update

--- run-example.sh
+++ run-example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zipline run --bundle my-db-bundle -f example/buy_and_hold.py --start 2016-1-1 --end 2018-1-1 -o dma.pickle
+zipline run --bundle my-db-bundle -f example/buy_and_hold.py --start 2016-1-1 --end 2017-11-30 -o dma.pickle
 
